4f8ae6a641529835c16473fe28df5bea68ae38fa
Adriano Gomes <69825351+AdrianoGomesFilho@users.noreply.github.com>
2024-12-03 20:00:55 -0300

    melhorando o código, indo direto ao PJE TST consulta de terceiros permite acessar tudo

--- 1astrea_pje_terceiros.py
+++ 1astrea_pje_terceiros.py
@@ -54,45 +54,14 @@ try:
             # Update the last clipboard content
             last_clipboard_content = paste
 
-            # Extract the TRT number (15th and 16th characters)
-            trt_number = paste[18:20]
-
-            # Remove leading zero if present
-            trt_number = trt_number.lstrip('0')
-
             # Construct the base URL dynamically
-            base_url = f"https://pje.trt{trt_number}.jus.br/primeirograu/login.seam"
+            base_url = f"https://pje.tst.jus.br/consultaprocessual/detalhe-processo/{paste}"
             
             # Find or open the tab for base_url
             base_url_handle = find_or_open_tab(driver, base_url)
             driver.switch_to.window(base_url_handle)
 
-            # Wait for the "modo-operacao" element to be present
-            modo_operacao_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "modo-operacao")))
-            modo_operacao_element.click()
-            buttons = driver.find_elements(By.XPATH, "//*[contains(@id, 'UtilizarPjeOffice')]")
-            button_id = buttons[0].get_attribute('id')
-            button_element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, button_id)))
-            driver.execute_script("arguments[0].scrollIntoView(true);", button_element)
-            WebDriverWait(driver, 300).until(EC.element_to_be_clickable((By.ID, button_id))).click()
-
-            # Wait for the "loginAplicacaoButton" button to be clickable and click it
-            WebDriverWait(driver, 300).until(EC.element_to_be_clickable((By.ID, "loginAplicacaoButton"))).click()
             
-            # Wait for login to complete before proceeding
-            WebDriverWait(driver, 300).until(EC.presence_of_element_located((By.CLASS_NAME, "nome-usuario")))
-            print(f"Login realizado (token)")
-
-            final_url = f"https://pje.trt{trt_number}.jus.br/consultaprocessual/detalhe-processo/{paste}"
-            
-            # Open the final URL in a new tab
-            driver.execute_script(f"window.open('{final_url}', '_blank');")
-
-            # Close the base_url tab
-            driver.close()
-
-            # Switch back to the original tab
-            driver.switch_to.window(driver.window_handles[0])
 
             
             #########################ASTREA######################################
