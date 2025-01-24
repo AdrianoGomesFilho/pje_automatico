1003406ce9971e81622f10eb2c14258ff889d950
Adriano Gomes <adrianogomesfilho98@gmail.com>
2025-01-24 15:46:47 -0300

    funcionando o astrea + pje via login, erro desconhecido

--- 1astrea_pje_terceiros_login_senha.py
+++ 1astrea_pje_terceiros_login_senha.py
@@ -152,18 +152,18 @@ try:
                 # Add the title and iframe to the end of the body element
                 driver.execute_script("""
                     var titleDiv = document.createElement('div');
-                    titleDiv.innerHTML = '<h2>Pré visualizador TST (sistema antigo)</h2>';
-                    titleDiv.style.marginTop = '700px';
+                    titleDiv.innerHTML = '<h2>Consulta no TST (sistema antigo)</h2>';
+                    titleDiv.style.marginTop = '550px';
                     document.body.appendChild(titleDiv);
 
                     var iframe = document.createElement('iframe');
                     iframe.src = arguments[0];
                     iframe.width = '100%';
                     iframe.height = '800px';
-                    iframe.style.border = 'none';
-                    iframe.style.marginTop = '100px';
-                    iframe.style.position = 'relative';  // Ensure the iframe is positioned relative to its container
+                    iframe.style.border = '2px solid black';
+                    iframe.style.marginTop = '20px';
                     document.body.appendChild(iframe);
+
                 """, iframe_url)
 
         except Exception as e:
