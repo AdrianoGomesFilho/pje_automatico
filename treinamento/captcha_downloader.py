6ceb5a1776461f8b3660155c85d7d2558cb67f08
Adriano Gomes <69825351+AdrianoGomesFilho@users.noreply.github.com>
2025-02-09 14:37:11 -0300

    add new images

--- treinamento/captcha_downloader.py
+++ treinamento/captcha_downloader.py
@@ -1,61 +1,48 @@
 import os
 import time
 import base64
-import logging
-from selenium import webdriver
+from selenium import webdriver #controla o browser
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By #usado para localizar elementos na página
+from selenium.webdriver.support.ui import WebDriverWait #usado para esperar (tempo) por condições específicas antes de executar o próximo comando
+from selenium.webdriver.support import expected_conditions as EC #usado para definir condições esperadas antes de executar o próximo comando
+from webdriver_manager.chrome import ChromeDriverManager #usado para instalar o driver do Chrome automaticamente, caso não esteja instalado
 
 url = "https://pje.trt1.jus.br/consultaprocessual/captcha/detalhe-processo/0100356-73.2020.5.01.0058/1"
-pasta_imagens = "C:/Users/fish/script_pje/treinamento"
+pasta_imagem = "C:/Users/fish/script_pje/treinamento/imagens"
 
-if not os.path.exists(pasta_imagens):
-    os.makedirs(pasta_imagens)
-
-logging.basicConfig(
-    level=logging.DEBUG,
-    filename='captcha_downloader.log',
-    filemode='w',
-    format='%(asctime)s - %(levelname)s - %(message)s'
-)
+if not os.path.exists(pasta_imagem):
+    os.makedirs(pasta_imagem) #cria a pasta caso ela não exista
 
 def download_captcha(driver):
-    logging.debug(f"Requisitando URL: {url}")
-    driver.get(url)
+    driver.get(url) #acessa a página do captcha
 
     try:
-        # Wait for the captcha image to be present
+        # Espera até que a imagem do captcha esteja presente
         captcha_img = WebDriverWait(driver, 5).until(
             EC.presence_of_element_located((By.ID, "imagemCaptcha"))
         )
-        img_src = captcha_img.get_attribute('src')
-        logging.debug(f"Captcha image src: {img_src}")
+        img_src = captcha_img.get_attribute('src') #pega o atributo src da imagem
 
         if img_src.startswith('data:image/png;base64,'):
-            img_data = img_src.split(',')[1]
-            img_data = base64.b64decode(img_data)
-            timestamp = int(time.time())
-            img_path = os.path.join(pasta_imagens, f'captcha_{timestamp}.png')
-            with open(img_path, 'wb') as f:
-                f.write(img_data)
-            print(f"Captcha downloaded and saved to {img_path}")
-            logging.info(f"Captcha downloaded and saved to {img_path}")
+            dados_imagem = img_src.split(',')[1] #pega a parte do base64 da imagem (1) (tudo que vier depois da virgula)
+            dados_imagem = base64.b64decode(dados_imagem)#decodifica a imagem usando a library base64
+            timestamp = int(time.time())#pega o timestamp atual, para que o nome do arquivo seja único, converte para integer para evitar pontos flutuantes
+            caminho_imagem = os.path.join(pasta_imagem, f'captcha_{timestamp}.png') #cria o caminho do arquivo
+            with open(caminho_imagem, 'wb') as arquivo: #abre o arquivo no modo de escrita binária (write binary - wb)
+                arquivo.write(dados_imagem) #escreve a imagem no arquivo
+            print(f"Imagem baixada e salva em {caminho_imagem}")
         else:
-            print("Captcha image source is not base64 encoded")
-            logging.error("Captcha image source is not base64 encoded")
+            print("A imagem não está codificada em base64")
     except Exception as e:
-        print("Captcha image not found")
-        logging.error("Captcha image not found")
-        logging.error(str(e))
+        print("A imagem não está codificada em base64")
+        print(str(e))
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install())) #instancia o driver do Chrome
 
 try:
-    while True:
+    while True: #o que deve ser true: o loop infinito, que só será interrompido manualmente
         download_captcha(driver)
-        time.sleep(1)
+        time.sleep(1)  #espera 1 segundo antes de executar o próximo comando
 finally:
-    driver.quit()
+    driver.quit() #fecha o browser
