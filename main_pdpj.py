3cffe4b5ea8d27f9adb2fbd8323189ae0d25329b
Adriano Gomes <adrianogomesfilho98@gmail.com>
2025-03-28 11:27:03 -0300

    estilizado

--- main_pdpj.py
+++ main_pdpj.py
@@ -38,7 +38,10 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from bs4 import BeautifulSoup
 
 # Path to the custom icon file
-ICON_PATH = os.path.join(os.path.dirname(__file__), "icon.ico")
+if getattr(sys, 'frozen', False):  # Check if running as a PyInstaller bundle
+    ICON_PATH = os.path.join(sys._MEIPASS, "icon.ico")
+else:
+    ICON_PATH = os.path.join(os.path.dirname(__file__), "icon.ico")
 
 # Update all references to the icon paths
 TKINTER_ICON_PATH = ICON_PATH
@@ -65,12 +68,19 @@ def run_tray_icon():
 tray_thread = threading.Thread(target=run_tray_icon, daemon=True)
 tray_thread.start()
 
+# Define color variables
+BACKGROUND_COLOR = "#D9CDFF"  # Light green background
+TEXT_COLOR = "#484554"       # Dark gray text
+BUTTON_BG_COLOR = "#ffc477"  # Yellow button background
+BUTTON_FG_COLOR = "#333222"  # Dark button text
+DISABLED_BUTTON_BG_COLOR = "#bcbba7"  # Gray button background for disabled buttons
+
 # Function to prompt user for credentials using a GUI and save them to a file
 def prompt_for_credentials(file_path, credentials, driver=None):
     main_window = tk.Tk()
     main_window.title("PJE Automático")
     main_window.attributes('-topmost', True)
-    main_window.configure(bg="#D9CDFF")  # Light green background
+    main_window.configure(bg=BACKGROUND_COLOR)  # Light green background
 
     # Set custom icon for the tkinter window
     main_window.iconbitmap(TKINTER_ICON_PATH)
@@ -85,33 +95,33 @@ def prompt_for_credentials(file_path, credentials, driver=None):
 
     font_style = ("Montserrat", 10)
 
-    tk.Label(main_window, text="E-mail do Astrea:", bg="#D9CDFF", fg="#484554", font=font_style).grid(row=0, column=0, padx=10, pady=5, sticky="e")
+    tk.Label(main_window, text="E-mail do Astrea:", bg=BACKGROUND_COLOR, fg=TEXT_COLOR, font=font_style).grid(row=0, column=0, padx=10, pady=5, sticky="e")
     username_astrea_entry = tk.Entry(main_window, width=40, font=font_style)
     username_astrea_entry.grid(row=0, column=1, padx=10, pady=5)
     username_astrea_entry.insert(0, credentials.get("USERNAMEASTREA", ""))
 
-    tk.Label(main_window, text="Senha do Astrea:", bg="#D9CDFF", fg="#484554", font=font_style).grid(row=1, column=0, padx=10, pady=5, sticky="e")
+    tk.Label(main_window, text="Senha do Astrea:", bg=BACKGROUND_COLOR, fg=TEXT_COLOR, font=font_style).grid(row=1, column=0, padx=10, pady=5, sticky="e")
     password_astrea_entry = tk.Entry(main_window, show='*', width=40, font=font_style)
     password_astrea_entry.grid(row=1, column=1, padx=10, pady=5)
     password_astrea_entry.insert(0, credentials.get("PASSWORDASTREA", ""))
 
-    tk.Label(main_window, text="CPF para login no PJE:", bg="#D9CDFF", fg="#484554", font=font_style).grid(row=2, column=0, padx=10, pady=5, sticky="e")
+    tk.Label(main_window, text="CPF para login no PJE:", bg=BACKGROUND_COLOR, fg=TEXT_COLOR, font=font_style).grid(row=2, column=0, padx=10, pady=5, sticky="e")
     username_pje_entry = tk.Entry(main_window, width=40, font=font_style)
     username_pje_entry.grid(row=2, column=1, padx=10, pady=5)
     username_pje_entry.insert(0, credentials.get("USERNAMEPJE", ""))
 
-    tk.Label(main_window, text="Senha para login no PJE:", bg="#D9CDFF", fg="#484554", font=font_style).grid(row=3, column=0, padx=10, pady=5, sticky="e")
+    tk.Label(main_window, text="Senha para login no PJE:", bg=BACKGROUND_COLOR, fg=TEXT_COLOR, font=font_style).grid(row=3, column=0, padx=10, pady=5, sticky="e")
     password_pje_entry = tk.Entry(main_window, show='*', width=40, font=font_style)
     password_pje_entry.grid(row=3, column=1, padx=10, pady=5)
     password_pje_entry.insert(0, credentials.get("PASSWORDPJE", ""))
 
-    tk.Label(main_window, text="Método de Login:", bg="#D9CDFF", fg="#484554", font=font_style).grid(row=4, column=0, padx=10, pady=5, sticky="e")
+    tk.Label(main_window, text="Método de Login:", bg=BACKGROUND_COLOR, fg=TEXT_COLOR, font=font_style).grid(row=4, column=0, padx=10, pady=5, sticky="e")
 
     login_method = tk.StringVar(value="Astrea + PJE (Senha)")
     methods = ["Somente PJE", "Astrea + PJE (Senha)", "Astrea + PJE (Token)", "Somente Astrea"]
     login_method = tk.StringVar(value=methods[0])  # Set the first option as pre-selected
     for i, method in enumerate(methods):
-        tk.Radiobutton(main_window, text=method, variable=login_method, value=method, bg="#D9CDFF", fg="#484554", font=font_style).grid(row=4 + i, column=1, sticky="w")
+        tk.Radiobutton(main_window, text=method, variable=login_method, value=method, bg=BACKGROUND_COLOR, fg=TEXT_COLOR, font=font_style).grid(row=4 + i, column=1, sticky="w")
 
     def save_and_run():
         username_pje = re.sub(r'\D', '', username_pje_entry.get())
@@ -137,7 +147,16 @@ def prompt_for_credentials(file_path, credentials, driver=None):
         main_window.destroy()
         run_script(credentials)
 
-    tk.Button(main_window, text="Iniciar", command=save_and_run, bg="#4CAF50", fg="white", width=15, font=font_style).grid(row=8, column=0, columnspan=2, pady=10)
+        with open(file_path, 'w') as cred_file:
+            json.dump(credentials, cred_file)
+
+        if driver:
+            driver.quit()
+
+        main_window.destroy()
+        run_script(credentials)
+
+    tk.Button(main_window, text="Iniciar", command=save_and_run, bg="#ffc477", fg="#333222", width=15, font=font_style).grid(row=8, column=0, columnspan=2, pady=10)
 
     main_window.mainloop()
     return credentials
@@ -146,7 +165,7 @@ def prompt_for_pje_level(paste):
     pje_level_window = tk.Tk()
     pje_level_window.title("Escolha o Grau")
     pje_level_window.attributes('-topmost', True)
-    pje_level_window.configure(bg="#e3f2fd")  # Light blue background
+    pje_level_window.configure(bg="#D9CDFF")  # Light blue background
 
     # Set custom icon for the tkinter window
     pje_level_window.iconbitmap(TKINTER_ICON_PATH)
@@ -161,8 +180,8 @@ def prompt_for_pje_level(paste):
 
     font_style = ("Montserrat", 12)
 
-    tk.Label(pje_level_window, text="Escolha o grau", bg="#e3f2fd", fg="#0d47a1", font=font_style).pack(pady=10)
-    tk.Label(pje_level_window, text=f"Processo que será aberto: {paste}", bg="#e3f2fd", fg="#0d47a1", font=font_style).pack(pady=10)
+    tk.Label(pje_level_window, text="Escolha o grau", bg="#D9CDFF", fg="#484554", font=font_style).pack(pady=10)
+    tk.Label(pje_level_window, text=f"Processo que será aberto: {paste}", bg="#D9CDFF", fg="#484554", font=font_style).pack(pady=10)
 
     pje_level = tk.StringVar()
 
@@ -170,10 +189,10 @@ def prompt_for_pje_level(paste):
         pje_level.set(level)
         pje_level_window.destroy()
 
-    tk.Button(pje_level_window, text="Primeiro Grau", command=lambda: select_level("Primeiro grau"), bg="#4CAF50", fg="white", width=20, font=font_style).pack(pady=5)
-    tk.Button(pje_level_window, text="Segundo Grau", command=lambda: select_level("Segundo grau"), bg="#2196F3", fg="white", width=20, font=font_style).pack(pady=5)
-    tk.Button(pje_level_window, text="TST", command=lambda: select_level("TST"), bg="#FF5722", fg="white", width=20, font=font_style).pack(pady=5)
-    tk.Button(pje_level_window, text="Ignorar e Aguardar", command=lambda: select_level("Ignore"), bg="#9E9E9E", fg="white", width=20, font=font_style).pack(pady=5)
+    tk.Button(pje_level_window, text="Primeiro Grau", command=lambda: select_level("Primeiro grau"), bg="#ffc477", fg="#333222", width=20, font=font_style).pack(pady=5)
+    tk.Button(pje_level_window, text="Segundo Grau", command=lambda: select_level("Segundo grau"), bg="#ffc477", fg="#333222", width=20, font=font_style).pack(pady=5)
+    tk.Button(pje_level_window, text="TST", command=lambda: select_level("TST"), bg="#ffc477", fg="#333222", width=20, font=font_style).pack(pady=5)
+    tk.Button(pje_level_window, text="Ignorar e Aguardar", command=lambda: select_level("Ignore"), bg="#bcbba7", fg="#333222", width=20, font=font_style).pack(pady=5)
 
     pje_level_window.mainloop()
     return pje_level.get()
