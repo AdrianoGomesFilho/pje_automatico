52ead2cd9f527a8f3edbb6317aed1bc37d56ae03
Adriano <adrianogomesfilho98@gmail.com>
2025-05-17 15:43:12 -0300

    reopen com alerta mais completo e organizado

--- pje_automatico.py
+++ pje_automatico.py
@@ -15,7 +15,7 @@ from plyer import notification
 from selenium.common.exceptions import NoSuchElementException
 from win10toast import ToastNotifier  # Add this import for Windows notifications
 
-CURRENT_VERSION = "1.1.0"
+CURRENT_VERSION = "1.1.1"
 
 UPDATE_URL = "https://raw.githubusercontent.com/AdrianoGomesFilho/pje_automatico/main/latest_version.json"
 
@@ -845,8 +845,8 @@ def prompt_reopen_pje(paste):
 
     screen_width = reopen_window.winfo_screenwidth()
     screen_height = reopen_window.winfo_screenheight()
-    window_width = 350
-    window_height = 260
+    window_width = 400
+    window_height = 300
     position_right = screen_width - window_width - 20 
     position_down = screen_height - window_height - 80  
     reopen_window.geometry(f"{window_width}x{window_height}+{position_right}+{position_down}")
@@ -855,7 +855,7 @@ def prompt_reopen_pje(paste):
     title_font_style = ("Montserrat", 14, "bold")
 
     tk.Label(reopen_window, text="Reabrir PJE", bg=BACKGROUND_COLOR, fg=TEXT_COLOR, font=title_font_style).pack(pady=10)
-    tk.Label(reopen_window, text=f"{paste}\nHouve algum erro, ou o processo não está cadastrado nessa instância. Certifique se o número copiado realmente existe!\nDeseja reabrir?", bg=BACKGROUND_COLOR, fg=TEXT_COLOR, font=font_style, wraplength=350, justify="center").pack(pady=10)
+    tk.Label(reopen_window, text=f"{paste}\nUm dos possíveis erros ocorreram:\n 1)Processo não cadastrado\n2)PJE não carregou completamente\n3)Número do processo não existe\nDeseja reabrir?", bg=BACKGROUND_COLOR, fg=TEXT_COLOR, font=font_style, wraplength=350, justify="center").pack(pady=10)
 
     def select_reopen(choice):
         nonlocal reopen_choice
